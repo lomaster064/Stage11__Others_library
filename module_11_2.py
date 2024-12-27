ed24fa28c95f4b2d44261631f88329108683c042
lomaster064 <64ilya64@gmail.com>
2024-12-27 12:53:27 -0800

    Add files via upload

---
+++ module_11_2.py
@@ -0,0 +1,43 @@
+import inspect
+
+
+def introspection(obj):
+    info = {
+        'type': type(obj).__name__,
+        'attributes': [],
+        'methods': [],
+        'module': inspect.getmodule(obj).__name__ if inspect.getmodule(obj) else '__main__'
+    }
+
+    for attribute_name in dir(obj):
+        attribute = getattr(obj, attribute_name)
+        if callable(attribute):
+            info['methods'].append(attribute_name)
+        else:
+            info['attributes'].append(attribute_name)
+
+    return info
+
+
+class Example:
+    def __init__(self):
+        self.attr1 = 'str'
+        self.attr2 = 123
+
+    def method1(self):
+        pass
+
+    def method2(self):
+        pass
+
+
+# Пример использования
+number_info = introspection(13)
+print(number_info)
+
+list_info = introspection(['asd', 123, [1, 2], False])
+print(list_info)
+
+example_obj = Example()
+example_info = introspection(example_obj)
+print(example_info)
